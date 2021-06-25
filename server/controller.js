df4912ee61f2ecd7c84dca68f5e9a6b164acfcde
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2021-06-26 00:55:35 +0300

    heroku

--- server/controller.js
+++ server/controller.js
@@ -3,29 +3,24 @@ const app = express()
 const port = 7600
 
 app.use(function (req, res, next) {
-
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
     // Pass to next layer of middleware
     next();
 });
 
 app.get('/tasks', (req, res) => {
-    const tasks = ['add new item', 'read book', 'running']
+    const tasks = ['add new item', 'read book', 'running', 'hello']
     res.send(tasks)
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`App listening at http://localhost:${port}`)
-})
+});
